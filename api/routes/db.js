427ab10c3985a658f0d90fa8116fbebf79ea8682
Jhony Souza <jhony.jss@gmail.com>
2020-07-13 10:38:59 -0300

    create user as dinamicly

--- api/routes/db.js
+++ api/routes/db.js
@@ -7,90 +7,90 @@ const { Router } = require('express')
 const db = new sqlite3.Database('db.sqlite')
 
 db.serialize(function () {
-    db.run(`CREATE TABLE if not exists users(
+  db.run(`CREATE TABLE if not exists users(
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             name text, 
             email text UNIQUE, 
             password text, 
             CONSTRAINT email_unique UNIQUE (email)
             )`);
-    /* var stmt = db.prepare('INSERT INTO lorem VALUES (?)')
+  /* var stmt = db.prepare('INSERT INTO lorem VALUES (?)')
 
-    for (var i = 0; i < 10; i++) {
-        stmt.run('Ipsum ' + i)
-    }
+  for (var i = 0; i < 10; i++) {
+      stmt.run('Ipsum ' + i)
+  }
 
-    stmt.finalize() */
+  stmt.finalize() */
 
-    /* db.each('SELECT rowid AS id, info FROM lorem', function (err, row) {
-        console.log(row.id + ': ' + row.info)
-    }) */
-    const router = Router()
+  /* db.each('SELECT rowid AS id, info FROM lorem', function (err, row) {
+      console.log(row.id + ': ' + row.info)
+  }) */
+  const router = Router()
 
-    router.use(express.json())
+  router.use(express.json())
 
 
-    /* router.post('/db', function (req, res, next) {
-        let sql = `INSERT INTO (users) VALUES (?,?,?,?)`;
-        db.run(insert, ["admin", "jhony.jss@gmail.com", md5("123456")])
-    }) */
-    //INSERT USERS
-    router.get('/db/user', function (req, res, next) {
-        res.json({ foo: 1 })
-      db.run('INSERT INTO users(name, email, password) VALUES(?, ?, ?)', ['Teste', 'teste@demo.com', md5('12345admin')], (err) => {
+  /* router.post('/db', function (req, res, next) {
+      let sql = `INSERT INTO (users) VALUES (?,?,?,?)`;
+      db.run(insert, ["admin", "jhony.jss@gmail.com", md5("123456")])
+  }) */
+  //INSERT USERS
+  router.post('/db/user', function (req, res, next) {
+    db.run('INSERT INTO users(name, email, password) VALUES(?, ?, ?)',
+      [req.body.name, req.body.email, md5(req.body.password)], (err) => {
         if (err) {
           return console.log(err.message);
         }
-        console.log(`Row was added to the table: ${this.lastID}`);
+        return res.json({ "message": "User Created" })
       })
+  })
+
+  router.get('/db/:id', function (req, res, next) {
+    let id = req.params.id
+    let sql = `SELECT * FROM users where id = ${id}`
+
+    db.get(sql, function (err, row) {
+      console.log(row.id + ': ' + row.info)
+      res.json(row)
     })
+  })
 
-    router.get('/db/:id', function (req, res, next) {
-        let id = req.params.id
-        let sql = `SELECT * FROM users where id = ${id}`
-       
-        db.get(sql, function (err, row) {
-            console.log(row.id + ': ' + row.info)
-            res.json(row)
-        })
-    })
-
-    router.post('/auth/login', function (req, res, next) {
-      if (req.body.password!='' && req.body.email!=''){
-        let sql = `SELECT * FROM users where email = '${req.body.email}' and password= '${md5(req.body.password)}'`
-        db.get(sql, function (err, row) {
-          if (err || typeof row=="undefined"){
-            res.status(401);
-            return res.end();
-          }
-          const payload = row;
-          const token = jwt.sign(payload, 'uploaddojhony');
-          req.session.userId = row.id
-          res.cookie('token', token);
-
-          return res.json({ token: token, data: row});
-
-        })
-      }
-    })
-
-    router.post("/auth/logout",(req, res, next)=>{
-      res.clearCookie("token");
-      return next()
-    })
-
-    router.get('/auth/user', (req,res,next)=>{
-      if(typeof req.session.userId=='undefined') {
-        jwt.destroy(res.cookie('token'))
-      }
-      let sql = `SELECT id, name, email FROM users where id = '${req.session.userId}'`
+  router.post('/auth/login', function (req, res, next) {
+    if (req.body.password != '' && req.body.email != '') {
+      let sql = `SELECT * FROM users where email = '${req.body.email}' and password= '${md5(req.body.password)}'`
       db.get(sql, function (err, row) {
-        if (err){
-          req.redirect('/')
+        if (err || typeof row == "undefined") {
+          res.status(401);
+          return res.end();
         }
-        return res.json({data: row });
-      })
-    })
+        const payload = row;
+        const token = jwt.sign(payload, 'uploaddojhony');
+        req.session.userId = row.id
+        res.cookie('token', token);
 
-    module.exports = router
+        return res.json({ token: token, data: row });
+
+      })
+    }
+  })
+
+  router.post("/auth/logout", (req, res, next) => {
+    res.clearCookie("token");
+    return next()
+  })
+
+  router.get('/auth/user', (req, res, next) => {
+    if (typeof req.session.userId == 'undefined') {
+      jwt.destroy(res.cookie('token'))
+    }
+    let sql = `SELECT id, name, email FROM users where id = '${req.session.userId}'`
+    db.get(sql, function (err, row) {
+      if (err) {
+        req.redirect('/')
+      }
+      return res.json({ data: row });
+    })
+  })
+
+  module.exports = router
 })
